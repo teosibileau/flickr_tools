1f35cdd74c65256a55a0eca88d0f42144aa1d1c4
teo <teo.sibileau@gmail.com>
2012-06-05 01:12:44 -0300

    updating

--- flickrtools/photosets/management/commands/scanfortags.py
+++ flickrtools/photosets/management/commands/scanfortags.py
@@ -7,6 +7,8 @@ import flickrapi
 from flickrauth.models import *
 from photosets.models import *
 from photosets.tasks import addPhoto
+from datetime import datetime
+
 class Command(BaseCommand):
     help='Command to scan new photos tagged with specific instances and asign them to sets'
     def handle(self,*args,**options):
@@ -16,15 +18,20 @@ class Command(BaseCommand):
             (token, frob) = flickr.get_token_part_one(perms='write')
             if not token: raw_input("Press ENTER after you authorized this program")
             flickr.get_token_part_two((token, frob))
-            for uset in Photoset.objects.filter(access=access):
+            # for photoset defined for this access
+            for uset in access.photosets.all():
                 # get photos with given tags
                 if uset.last_checked:
+                    # if last_checked instance use it to narrow picture search
                     photos=flickr.walk(user_id=access.user,tags=uset.tags,tag_mode='AND',min_taken_time=uset.last_checked)
                 else:
+                    # if not bring them all
                     photos=flickr.walk(user_id=access.user,tags=uset.tags,tag_mode='AND')
                 photos=[photo for photo in photos]
                 if not uset.uid:
                     uset.uid=flickr.photosets_create(title=uset.title,primary_photo_id=photos[0].get('id'))[0].get('id')
                 for photo in photos:
                     addPhoto.delay(access,uset,photo)
+                # Save Timestamp to photoset instance
+                uset.last_checked=datetime.now()
                 uset.save()
--- flickrtools/photosets/tasks.py
+++ flickrtools/photosets/tasks.py
@@ -1,14 +1,19 @@
 from celery.task import task
-
+from django.core import serializers
 import flickrapi
+import string
+import sys
 
 @task
 def addPhoto(access,uset,photo):
-    print "adding %s in set %s for %s access"%(photo.get('id'),uset.uid,access.user)
     flickr=flickrapi.FlickrAPI(access.key,access.secret)
-    flickr.photos_setPerms(photo_id=photo.get('id'),is_public=1,is_friend=0,is_family=0,perm_comment=3,perm_addmeta=0)
+    (token, frob) = flickr.get_token_part_one(perms='write')
+    if not token: raw_input("Press ENTER after you authorized this program")
+    flickr.get_token_part_two((token, frob))
+    if not flickr.photos_getPerms(photo_id=photo.get('id'))[0].get('ispublic'):
+        flickr.photos_setPerms(photo_id=photo.get('id'),is_public=1,is_friend=0,is_family=0,perm_comment=3,perm_addmeta=0)
     try:
         response=flickr.photosets_addPhoto(photoset_id=uset.uid,photo_id=photo.get('id'))
-    except:
-        response='Something went grong'
+    except flickrapi.exceptions.FlickrError as error:
+        response=error
     return response
